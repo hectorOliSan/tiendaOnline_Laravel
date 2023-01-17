a208f0118f36aa67ed163518d6fcd8a8f020a7a7
Héctor Olivares Sánchez <hectorolivares35@gmail.com>
2023-01-17 10:38:44 +0000

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,16 +6,19 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    public function index() {
+        $viewData = [];
+        $viewData["title"] = "Página principal - E Code";
+        return view('home.index')->with("viewData", $viewData);
+    }
+
     public function about()
     {
-        $title = "Acerca de nosotros - E Code";
-        $subtitle = "Acerca de nosotros";
-        $description = "Contenido de la página acerca de...";
-        $author = "Héctor Olivares Sánchez";
-
-        return view("home.about")->with("title", $title)
-            ->with("subtitle", $subtitle)
-            ->with("description", $description)
-            ->with("author", $author);
+        $viewData = [];
+        $viewData["title"] = "Acerca de nosotros - E Code";
+        $viewData["subtitle"] = "Acerca de nosotros";
+        $viewData["description"] = "Contenido de la página acerca de...";
+        $viewData["author"] = "Héctor Olivares Sánchez";
+        return view("home.about")->with("viewData", $viewData);
     }
 }

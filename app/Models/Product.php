c0d53c0f5d48896017f1f48687171314872b044c
Héctor Olivares Sánchez <hectorolivares35@gmail.com>
2023-01-31 11:44:33 +0000

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,42 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'description', 'image', 'price'];
+
+    public function getID(): int{
+        return $this -> attributes["id"];
+    }
+
+    public function getName(): String{
+        return $this -> attributes["name"];
+    }
+
+    public function setName(String $name): void{
+        $this -> attributes["name"] = $name;
+    }
+
+    public function getDescription(): String{
+        return $this -> attributes["description"];
+    }
+
+    public function setDescription(String $description): void{
+        $this -> attributes["description"] = $description;
+    }
+
+    public function getImage(): String{
+        return $this -> attributes["image"];
+    }
+
+    public function setImage(String $image): void{
+        $this -> attributes["image"] = $image;
+    }
+
+    public function getPrice(): float{
+        return $this -> attributes["price"];
+    }
+
+    public function setPrice(float $price): void{
+        $this -> attributes["price"] = $price;
+    }
 }

8c805846b5d04deb8354680137b248b4a9d2eaf2
Héctor Olivares Sánchez <hectorolivares35@gmail.com>
2023-01-31 11:44:35 +0000

    Update AdminProductController.php

--- app/Http/Controllers/Admin/AdminProductController.php
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -24,21 +24,57 @@ class AdminProductController extends Controller
             "name" => "required|max:255",
             "description" => "required",
             "price" => "required|decimal:0,2|min:1",
-            "image" => "required"
+            "image" => "image|mimes:jpeg,jpg,png,gif,svg"
         ]);
-
-        $lastID = DB::select("SHOW TABLE STATUS LIKE 'products'")[0]->Auto_increment;
-        $extension = $request->file('image')->extension();
-        Storage::disk('public')->put(
-            $lastID.".".$extension,
-            file_get_contents($request->file('image')->getRealPath())
-        );
 
         $product = new Product();
         $product->name = $request->input('name');
         $product->description = $request->input('description');
         $product->price = $request->input('price');
-        $product->image = $lastID.".".$extension;
+        $product->image = 'image.';
+        $product->save();
+
+        if($request -> hasFile("image")){
+            $imageName =  $product->id.".".$request->image->extension();
+            $product->setImage($imageName);
+            $product->save();
+        };
+
+        Storage::disk('public')->put(
+            $imageName,
+            file_get_contents($request->file('image')->getRealPath())
+        );
+
+        return redirect()->route('admin.product.index');
+    }
+
+    public function delete(int $id) {
+        Storage::disk('public')->delete(Product::find($id)->image);
+        Product::destroy($id);
+        return redirect()->route('admin.product.index');
+    }
+
+    public function edit(int $id) {
+        $viewData = [];
+        $viewData["title"] = "Panel de control - E Code";
+        $viewData["product"] = Product::find($id);
+        return view('admin.products.edit')->with("viewData", $viewData);
+    }
+
+    public function update(int $id, Request $request) {
+        $product = Product::findOrFail($id);
+        $product->update($request->all());
+
+        if($request -> hasFile("image")){
+            Storage::disk('public')->delete(Product::find($id)->image);
+            $imageName =  $product->id.".".$request->image->extension();
+            $product -> setImage($imageName);
+            Storage::disk('public')->put(
+                $imageName,
+                file_get_contents($request->file('image')->getRealPath())
+            );
+        }
+
         $product->save();
         return redirect()->route('admin.product.index');
     }
